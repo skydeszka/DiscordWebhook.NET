829364245342e8e802d1152f35708622ff376a31
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-01 13:41:36 +0200

    Create EmbedThumbnail.cs

---
+++ Entities/Embeds/EmbedThumbnail.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedThumbnail
+{
+    [JsonProperty("url")]
+    public string Url { get; private set; } = string.Empty;
+
+    [JsonProperty("proxy_url")]
+    public string ProxyUrl { get; private set; } = string.Empty;
+
+    [JsonProperty("height")]
+    public int Height { get; private set; }
+
+    [JsonProperty("width")]
+    public int Width { get; private set; }
+}
