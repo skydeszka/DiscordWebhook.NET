59592e91aa71de9defd3d1a97b1a2779eea212ef
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-01 12:30:44 +0200

    Create ExecuteWebhookQuery.cs

---
+++ Entities/ExecuteWebhookQuery.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordWebhook.Entities;
+
+public struct ExecuteWebhookQuery
+{
+    public bool? wait = null;
+    public ulong? threadId = null;
+
+    public ExecuteWebhookQuery(ulong thread_id) =>
+        threadId = thread_id;
+
+    public ExecuteWebhookQuery(bool wait) =>
+        this.wait = wait;
+
+    public ExecuteWebhookQuery(ulong thread_id, bool wait)
+    {
+        threadId = thread_id;
+        this.wait = wait;
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder('?');
+        if (wait is not null)
+        {
+            builder.Append("wait=");
+            builder.Append(wait);
+            builder.Append('&')
+        }
+
+        if(threadId is not null)
+        {
+            builder.Append("thread_id=");
+            builder.Append(threadId);
+        }
+
+        return builder.ToString();
+    }
+}
