497fe97dc01c50ff0802998a356a11baec3b3acb
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 14:36:50 +0200

    Create AllowedMention.cs

---
+++ Entities/AllowedMention.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities;
+
+public struct AllowedMention
+{
+    [JsonProperty("parse")]
+    public IList<AllowedMentionType>? Parse { get; set; } = null;
+
+    [JsonProperty("roles")]
+    public IList<ulong>? Roles { get; set; } = null;
+
+    [JsonProperty("users")]
+    public IList<ulong>? Users { get; set; } = null;
+
+    [JsonProperty("replied_user")]
+    public bool RepliedUser { get; set; } = false;
+
+    public AllowedMention() { }
+}
