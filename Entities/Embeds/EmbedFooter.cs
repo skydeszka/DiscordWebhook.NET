ebdb10bcbca940829f38c01ee28d827149375955
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 07:08:58 +0200

    Create EmbedFooter.cs

---
+++ Entities/Embeds/EmbedFooter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedFooter
+{
+    [JsonProperty("text")]
+    public string Text { get; set; } = string.Empty;
+
+    [JsonProperty("icon_url")]
+    public string? IconUrl { get; set; }
+
+    [JsonProperty("proxy_icon_url")]
+    public string? ProxyIconUrl { get; set; }
+}
