bb2e2a635570ba6a5393540f1a4f2f9e6159a552
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 07:06:32 +0200

    Create EmbedAuthor.cs

---
+++ Entities/Embeds/EmbedAuthor.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedAuthor
+{
+    [JsonProperty("name")]
+    public string Name { get; private set; } = string.Empty;
+
+    [JsonProperty("url")]
+    public string? Url { get; private set; }
+
+    [JsonProperty("icon_url")]
+    public string? IconUrl { get; private set; }
+
+    [JsonProperty("proxy_icon_url")]
+    public string? ProxyIconUrl { get; private set; }
+}
