ba33dcf299bbbd31ebdfff4b62e301b80d6d9fab
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 06:32:04 +0200

    Create EmbedImage.cs

---
+++ Entities/Embeds/EmbedImage.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedImage
+{
+    [JsonProperty("url")]
+    public string Url { get; private set; } = string.Empty;
+
+    [JsonProperty("proxy_url")]
+    public string? ProxyUrl { get; private set; }
+
+    [JsonProperty("height")]
+    public int? Height { get; private set; } = null;
+
+    [JsonProperty("width")]
+    public int? Width { get; private set; } = null;
+}
