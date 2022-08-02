11115379cb0b991c9792e1eb96978711716b8b1b
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 07:09:01 +0200

    Create EmbedField.cs

---
+++ Entities/Embeds/EmbedField.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedField
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonProperty("value")]
+    public string Value { get; set; } = string.Empty;
+
+    [JsonProperty("inline")]
+    public bool? Inline { get; set; } = null;
+}
