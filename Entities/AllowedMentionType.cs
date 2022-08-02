215d05849fe32cd8de80ad6b9f2f30e0fdd393e2
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 14:36:48 +0200

    Create AllowedMentionType.cs

---
+++ Entities/AllowedMentionType.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.SmartEnum;
+
+namespace DiscordWebhook.Entities;
+
+public sealed class AllowedMentionType : SmartEnum<AllowedMentionType>
+{
+    public static readonly AllowedMentionType RoleMentions = new("roles", 1);
+    public static readonly AllowedMentionType UserMentions = new("users", 2);
+    public static readonly AllowedMentionType EveryoneMentions = new("everyone", 3);
+
+    public AllowedMentionType(string name, int value) : base(name, value)
+    {
+    }
+}
