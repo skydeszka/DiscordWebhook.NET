8cf219ea97493ebf0d65b03da90dd0eebffe6b51
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 15:33:11 +0200

    Create Attachment.cs

---
+++ Entities/Attachment.cs
@@ -0,0 +1,15 @@
+﻿namespace DiscordWebhook.Entities;
+
+public struct Attachment
+{
+    public ulong Id { get; set; }
+    public string Filename { get; set; }
+    public string? Description { get; set; }
+    public string? ContentType { get; set; }
+    public int Size { get; set; }
+    public string Url { get; set; }
+    public string? ProxyUrl { get; set; }
+    public int? Height { get; set; }
+    public int? Width { get; set; }
+    public bool? Ephemeral { get; set; }
+}
