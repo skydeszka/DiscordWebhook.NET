3790e5113c9b090c33b0cfec8c5f89a78103bf71
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 08:32:00 +0200

    Create WebhookMessageOptions.cs

---
+++ Entities/WebhookMessageOptions.cs
@@ -0,0 +1,24 @@
+﻿using DiscordWebhook.Entities.Embeds;
+
+namespace DiscordWebhook.Entities;
+
+public struct WebhookMessageOptions
+{
+    //Query String Params
+    public bool Wait = false;
+    public ulong? ThreadId = null;
+
+    //JSON/Form Params
+    public string? Content = null;
+    public string? Username = null;
+    public string? AvatarUrl = null;
+
+    public bool Tts = false;
+    public IList<Embed>? Embeds = null;
+    // TODO: Implement AllowedMentions class
+    //public AllowedMentions? AllowedMentions = null;
+    // TODO: Implement rest of params
+    // https://discord.com/developers/docs/resources/webhook#execute-webhook
+
+    public WebhookMessageOptions() { }
+}
