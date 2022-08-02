b0594b945e7eb153dd375de556646354751d1803
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 07:04:09 +0200

    Create EmbedProvider.cs

---
+++ Entities/Embeds/EmbedProvider.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace DiscordWebhook.Entities.Embeds;
+
+public class EmbedProvider
+{
+    [JsonProperty("name")]
+    public string? Name { get; private set; }
+
+    [JsonProperty("url")]
+    public string? Url { get; private set; }
+}
