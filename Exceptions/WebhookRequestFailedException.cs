49697ae968b2cc7e8c41072e680d0eaab3be971c
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-02 10:13:36 +0200

    added new exceptions

---
+++ Exceptions/WebhookRequestFailedException.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace DiscordWebhook.Exceptions;
+[Serializable]
+internal class WebhookRequestFailedException : Exception
+{
+    public WebhookRequestFailedException(HttpResponseMessage response)
+        : base(response.Content.ReadAsStringAsync().Result)
+    {
+    }
+
+    public WebhookRequestFailedException(HttpResponseMessage response, Exception? innerException)
+        : base(response.Content.ReadAsStringAsync().Result, innerException)
+    {
+    }
+
+    protected WebhookRequestFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
