32d686d226e9924da1f2949d9a8f083ee9290c9c
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:14:01 +0200

    Create SelectOption.cs

---
+++ Entities/Components/SelectOption.cs
@@ -0,0 +1,14 @@
+﻿namespace DiscordWebhook.Entities.Components;
+
+public class SelectOption
+{
+    public string Label { get; set; } = string.Empty;
+
+    public string Value { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    // TODO: emoji
+
+    public bool Default { get; set; }
+}
