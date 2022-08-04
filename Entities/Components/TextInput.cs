6beb0d94ce76eaa252cd29ebb9ac0eaf3e9a5705
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:27:26 +0200

    Create TextInput.cs

---
+++ Entities/Components/TextInput.cs
@@ -0,0 +1,22 @@
+﻿namespace DiscordWebhook.Entities.Components;
+
+public class TextInput : IComponent
+{
+    public ComponentType Type => ComponentType.TextInput;
+
+    public string? CustomId { get; set; }
+
+    public TextInputStyle Style { get; set; } = TextInputStyle.Short;
+
+    public string Label { get; set; } = string.Empty;
+
+    public int? MinLength { get; set; }
+
+    public int? MaxLength { get; set; }
+
+    public bool Required { get; set; }
+
+    public string? Value { get; set; }
+
+    public string? Placeholder { get; set; }
+}
