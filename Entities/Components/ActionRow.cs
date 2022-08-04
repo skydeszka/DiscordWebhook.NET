087e84e8aba56bac76e6427b85b71eb010d58a5f
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:33:47 +0200

    Create ActionRow.cs

---
+++ Entities/Components/ActionRow.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscordWebhook.Entities.Components;
+
+public class ActionRow : IComponent
+{
+    public ComponentType Type => ComponentType.ActionRow;
+
+    public IList<ICustomComponent>? Components { get; set; }
+}
