b52a59fcf04775932fc3e91932709604ed77b6fa
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:27:23 +0200

    Create TextInputStyle.cs

---
+++ Entities/Components/TextInputStyle.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.SmartEnum;
+
+namespace DiscordWebhook.Entities.Components;
+
+public class TextInputStyle : SmartEnum<TextInputStyle>
+{
+    public static readonly TextInputStyle Short = new("short", 1);
+
+    public static readonly TextInputStyle Paragraph = new("paragraph", 2);
+
+    public TextInputStyle(string name, int value) : base(name, value)
+    {
+    }
+}
