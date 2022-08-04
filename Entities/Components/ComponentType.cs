d2d28f2541b9b536b66e05460eef8fb5f55abb36
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:06:52 +0200

    Create ComponentType.cs

---
+++ Entities/Components/ComponentType.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.SmartEnum;
+
+namespace DiscordWebhook.Entities.Components;
+
+public sealed class ComponentType : SmartEnum<ComponentType>
+{
+    public static readonly ComponentType ActionRow = new(nameof(ActionRow), 1);
+    public static readonly ComponentType Button = new(nameof(Button), 2);
+    public static readonly ComponentType SelectMenu = new(nameof(SelectMenu), 3);
+    public static readonly ComponentType TextInput = new(nameof(TextInput), 4);
+
+    public ComponentType(string name, int value) : base(name, value)
+    {
+    }
+}
