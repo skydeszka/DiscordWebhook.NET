781b30dca17910906b181dbe7e7920fe576d5e98
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-08-04 20:14:04 +0200

    Create SelectMenu.cs

---
+++ Entities/Components/SelectMenu.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscordWebhook.Entities.Components;
+
+public class SelectMenu : IComponent
+{
+    public ComponentType Type => ComponentType.SelectMenu;
+
+    public string? CustomId { get; set; }
+
+    public IList<SelectOption>? Options { get; set; }
+
+    public string? Placeholder { get; set; }
+
+    public int? MinValues { get; set; }
+
+    public int? MaxValues { get; set; }
+
+    public bool Disabled { get; set; }
+}
